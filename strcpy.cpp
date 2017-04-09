8433897fd9e821dba6a1942551bbaecfa372a502
xupu <xupo@mail2.sysu.edu.cn>
2017-04-09 13:07:17 +0800

    upload again

---
+++ strcpy.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+	char x[] = "Happy birthday to you";
+	char y[25];
+	char z[15];
+	printf("%s%s\n%s%s\n","The string in array x is: ", x, "The string in array y is: ", strcpy(y,x));
+	strncpy(z,x,14);
+	z[14]='\0';
+	printf("The string in array z is: %s\n",z);
+	return 0;
+}
